ae9352b3139f7dd630dd36cc91381036ef545524
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2021-01-06 18:59:49 -0500

    interface RM

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -4,5 +4,7 @@ interface DataReader {
 }
 
 export class MatchReader {
+  
+  /* *** reader is something (object literal, object of a class, etc. anything ) that must have a read method returning void and a data property of type string[][] **** */
   constructor(public reader: DataReader) {}
 }
